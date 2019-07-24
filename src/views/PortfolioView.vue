6d5a24786cb53fee99bf75d202aeeb11d4a4b313
이형원 <zettank@naver.com>
2019-07-24 16:26:29 +0900

    asd

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -37,6 +37,9 @@
         <v-icon size="17" class="mr-2">fa-undo</v-icon>Back
       </v-btn>
     </v-flex>
+    <v-flex xs12>
+      <div id="disqus_thread"></div>
+    </v-flex>
   </v-container>
 </div>
 </template>
@@ -73,6 +76,21 @@ export default {
 
     });
     this.id = this.$route.params.id;
+    /**
+    *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+    *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+
+    var disqus_config = function () {
+    this.page.url = 'http://70.12.247.78:8080/post';  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = 'ssafyeeasy'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    };
+
+    (function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://ssafyeeasy.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
   },
 
   components: {
